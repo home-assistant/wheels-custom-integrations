a2151abfa56ba616f238c45122bfea0b1ab9bb4a
Martin Hjelmare <marhje52@gmail.com>
2020-08-28 15:53:06 +0200

    Add CI and script base

---
+++ scripts/validate_requirements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Validate custom wheels package requirements."""
+import sys
+
+
+def main() -> int:
+    """Run main function."""
+    print("Validating requirements")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
